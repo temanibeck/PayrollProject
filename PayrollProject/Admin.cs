a8f39a6e2e61af0ae59bfbb58dedf3782670a77b
Temani Beck <temanibeck@gmail.com>
2018-07-31 16:29:02 -0500

    Created fields, properties, and methods for Admin class

--- PayrollProject/Admin.cs
+++ PayrollProject/Admin.cs
@@ -3,8 +3,32 @@ namespace PayrollProject
 {
     public class Admin : Staff
     {
-        public Admin()
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30f;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate){}
+
+        public override void CalculatePay()
         {
+            base.CalculatePay();
+            Allowance = 1000;
+
+            if (HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (HoursWorked - 160);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff
+                + "\nadminHourlyRate = " + adminHourlyRate +
+                "\nHoursWorked = " + HoursWorked
+                + "\nBasicPay = " + BasicPay +
+                "\nOvertime = " + Overtime +
+                "\n\nTotalPay = " + TotalPay;
         }
     }
 }
