7456090959343e34ee6ae65439db794be70fe0e6
Temani Beck <temanibeck@gmail.com>
2018-07-31 16:16:37 -0500

    Created fields, properties, and methods for Manager class

--- PayrollProject/Manager.cs
+++ PayrollProject/Manager.cs
@@ -7,9 +7,24 @@ namespace PayrollProject
         public int Allowance { get; private set; }
 
 
-        public Manager(string name)
+        public Manager(string name) : base(name, managerHourlyRate){}
+
+        public override void CalculatePay()
         {
-            
+            base.CalculatePay();
+            Allowance = 1000;
+            if (HoursWorked > 160)
+                TotalPay = BasicPay + Allowance;
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff
+                + "\nmanagerHourlyRate = " + managerHourlyRate +
+                "\nHoursWorked = " + HoursWorked
+                + "\nBasicPay = " + BasicPay +
+                "\nAllowance = " + Allowance +
+                "\n\nTotalPay = " + TotalPay;
         }
     }
 }
