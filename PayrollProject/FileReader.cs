05f6aa05f90be0bc1d938fc11a4de3cc1eb48a50
Temani Beck <temanibeck@gmail.com>
2018-07-31 20:38:38 -0500

    Created staff list, string array, and method for FileReader class

--- PayrollProject/FileReader.cs
+++ PayrollProject/FileReader.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.IO;
+
 namespace PayrollProject
 {
     public class FileReader
     {
-        public FileReader()
+        public List<Staff> ReadFile()
         {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.txt";
+            string[] separator = { ", " };
+
+            if (File.Exists(path))
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        result = sr.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                        if (result[1] == "Manager")
+                            myStaff.Add(new Manager(result[0]));
+
+                        else if (result[1] == "Admin")
+                            myStaff.Add(new Admin(result[0]));
+                    }
+                    sr.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: File does not exist");
+            }
+
+            return myStaff;
         }
     }
 }
