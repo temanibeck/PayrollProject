e2239afdec75ec236e687833418a41132fccebe7
Temani Beck <temanibeck@gmail.com>
2018-08-09 14:21:17 -0500

    Created fields, an enum, a constuctor method, and other methods for PaySlip class

--- PayrollProject/PaySlip.cs
+++ PayrollProject/PaySlip.cs
@@ -1,10 +1,76 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace PayrollProject
 {
     public class PaySlip
-    {
-        public PaySlip()
+    { 
+        private int month;
+        private int year;
+
+        enum MonthsOfYear { JAN = 1, FEB = 2, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }
+
+        public PaySlip(int payMonth, int payYear)
         {
+            month = payMonth;
+            year = payYear;
         }
-    }
+
+        public void GeneratePaySlip(List<Staff> myStaff)
+        {
+            string path;
+
+            foreach (staff f in myStaff)
+            {
+                path = f.NameOfStaff + ".txt.";
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
+                    sw.WriteLine("====================");
+                    sw.WriteLine("Name of Staff: {0}", f.NameOfStaff);
+                    sw.WriteLine("Hours Worked: {0}", f.HoursWorked);
+                    sw.WriteLine("");
+                    sw.WriteLine("Basic Pay: {0;C}", f.BasicPay);
+
+                    if (f.GetType() == typeof(Manager))
+                        sw.WriteLine("Allowance: {0;C}", ((Manager)f).Allowance);
+                    else if (f.GetType() == typeof(Admin))
+                        sw.WriteLine("Overtime: {0:C}", ((Admin)f).Overtime);
+
+                    sw.WriteLine("");
+                    sw.WriteLine("====================");
+                    sw.WriteLine("Total Pay: {0;C}", f.TotalPay);
+
+                    sw.Close();
+                }
+            }
+        }
+
+        public void GenerateSummary(List<Staff> myStaff)
+        {
+            var result = from f in myStaff
+                         where f.HoursWorked < 10
+                         orderby f.NameOfStaff ascending
+                         select new { f.NameOfStaff, f.HoursWorked };
+
+            string path = "summary.txt";
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("Staff with less than 10 working hours");
+                sw.WriteLine("");
+
+                foreach (var f in result)
+                    sw.WriteLine("Name of Staff: {0}, Hours Worked: {1}", f.NameOfStaff, f.HoursWorked);
+                sw.Close();
+            }
+
+            public override string ToString()
+            {
+                return "month = " + month + "year = " + year;
+            }
+        }
+    }     
 }
